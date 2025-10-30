e5c1c427304a6b57d403fc552adee90bf2ab52a8
diayan <diayans@outlook.com>
2025-10-30 17:13:02 +0000

    Initial Commit

---
+++ WishKit/WishKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishKitApp.swift
+//  WishKit
+//
+//  Created by diayan siat on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct WishKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WishKitTests/WishKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  WishKitTests.swift
+//  WishKitTests
+//
+//  Created by diayan siat on 30/10/2025.
+//
+
+import Testing
+@testable import WishKit
+
+struct WishKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
